708e07f3444d8de240f4ce54852df020dde78ac3
BielGR10 <gabriel.alves.martins.2010@gmail.com>
2026-09-10 15:43:40 -0300

    feat: Adicionado exercício 14

---
+++ src/main/java/ex014.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ex014{
+    static void main(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o seu saldo médio mensal: R$");
+        float saldo = input.nextFloat();
+
+        if(saldo > 5000){
+            System.out.println("Você tem direito a isenção de tarifa bancária!");
+        }else{
+            System.out.println("A sua tarifa bancária será mantida.");
+        }
+
+
+    }
+}

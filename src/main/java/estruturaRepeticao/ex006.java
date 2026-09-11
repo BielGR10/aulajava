a9dd315d1ff554eb44c14d9709e7afaa0f0d3c4a
BielGR10 <gabriel.alves.martins.2010@gmail.com>
2026-09-11 14:42:41 -0300

    Feat: Adicionado exercicio 6 de estrutura de repetição

---
+++ src/main/java/estruturaRepeticao/ex006.java
@@ -0,0 +1,13 @@
+package estruturaRepeticao;
+import java.util.Scanner;
+
+public class ex006 {
+    static void main(){
+        Scanner input = new Scanner(System.in);
+
+        for(int quantidade = 1; quantidade <= 15; quantidade++){
+            System.out.println("Produto nº"+quantidade+" verificado.");
+        }
+    }
+
+}

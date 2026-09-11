904681cc7db6d7addc73855f7847c9236f92f708
BielGR10 <gabriel.alves.martins.2010@gmail.com>
2026-09-11 13:53:29 -0300

    Feat: Adicionado exercicio 1 de estrutura de repetição

---
+++ src/main/java/estruturaRepeticao/ex001.java
@@ -0,0 +1,17 @@
+package estruturaRepeticao;
+import java.util.Scanner;
+
+public class ex001 {
+    static void main(){
+        Scanner input = new Scanner(System.in);
+
+
+        for(int i=1;i <= 10;i++){
+            System.out.println("Participante "+i+" registrado.");
+        }
+
+
+
+    }
+
+}
